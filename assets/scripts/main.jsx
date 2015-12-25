e5fc3ec6b51cf49786f486ecd7a988d59055f58d
Evan Krambuhl <thekrambuhl@gmail.com>
2015-12-24 19:19:25 -0800

    simply tasks

--- assets/scripts/main.jsx
+++ assets/scripts/main.jsx
@@ -2,6 +2,6 @@ import fetch from 'whatwg-fetch';
 
 import React from 'react';
 import { render } from 'react-dom';
-import Router from '../../components/router.jsx';
+import Router from '../../components/router';
 
 render(Router, document.querySelector('#il7'));
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gulp = require('gulp');
 var sequence = require('run-sequence');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
 
 // style plugins
 var sass = require('gulp-sass');
@@ -15,30 +14,23 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-
- 
-// Task `empty`
-// empties the dist folder before each startup
-gulp.task('empty', function(done) {
-  del('dist/**/*', done)
-});
  
 // Task `styles`
 // compiles stylesheet and optimises file 
 gulp.task('styles', function() {
-  return gulp.src('source/styles/*.scss')
+  return gulp.src('./assets/styles/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
       autoprefixer({ browsers: ['last 2 version'] })
     ]))
-    .pipe(gulp.dest('dist/assets'));
+    .pipe(gulp.dest('assets'));
 });
  
 // Task `scripts`
 // compiles app script and optimises files 
 gulp.task('scripts', function() {
   var b = browserify({ 
-    entries: './source/scripts/main.jsx',
+    entries: './assets/scripts/main.jsx',
     debug: true,
     extensions: ['.js', '.jsx']
   });
@@ -50,57 +42,21 @@ gulp.task('scripts', function() {
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/assets'));
+    .pipe(gulp.dest('./assets'));
 });
- 
-// Task `copy-images`
-// minimizes and copies images
-gulp.task('copy-images', function() {
-  return gulp.src('source/images/**/*')
-    .pipe(gulp.dest('dist/assets/images'));
-});
-
-// Task `copy-svg`
-// copies svg folder to dist
-gulp.task('copy-svg', function() {
-  return gulp.src('source/svg/**/*')
-    .pipe(gulp.dest('dist/assets/svg'));
-});
- 
-// Task `copy-fonts`
-// copies fonts folder to dist
-gulp.task('copy-fonts', function() {
-  return gulp.src('source/fonts/**/*')
-    .pipe(gulp.dest('dist/assets/fonts'));
-});
-
-// Task `copy-pages`
-// copies pages folder to dist
-gulp.task('copy-pages', function() {
-  return gulp.src('source/index.html')
-    .pipe(gulp.dest('dist/'));
-});
-
  
 // Task `watch`
 // run various tasks on file changes
 gulp.task('watch', function () {
-  gulp.watch('source/styles/**/*', ['styles']);
-  gulp.watch('source/scripts/**/*', ['scripts']);
+  gulp.watch('assets/styles/**/*', ['styles']);
+  gulp.watch('assets/scripts/**/*', ['scripts']);
   gulp.watch('components/**/*', ['scripts']);
-  gulp.watch('source/images/**/*', ['copy-images']);
-  gulp.watch('source/svg/**/*', ['copy-svg']);
-  gulp.watch('source/pages/**/*', ['copy-pages']);
 });
-
-// Task `copy`
-// copies assets
-gulp.task('copy', ['copy-images', 'copy-fonts', 'copy-pages', 'copy-svg']);
  
 // Task `compile`
 // Deletes dist folder and builds site from scratch
 gulp.task('compile', function(done) {
-  sequence('empty', ['styles', 'scripts', 'copy'], done);
+  sequence(['styles', 'scripts'], done);
 });
  
 // Task `develop`
